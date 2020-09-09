22e9fad39825aae78991a45c97c8a5b09e7e8c01
ojs00831 <ojs00831@naver.com>
2020-09-09 12:10:46 +0900

    Updates

---
+++ precache-manifest.9f20b9ff28e13473f1695a42f4d3caa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3131a606ad717e8da3e1e3b6f10dd235",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68b49a3af3eac5fd926b",
+    "url": "/static/css/main.0b117d5a.chunk.css"
+  },
+  {
+    "revision": "dcbc1ad3746847d335f9",
+    "url": "/static/js/2.968ebb96.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.968ebb96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b49a3af3eac5fd926b",
+    "url": "/static/js/main.cd083e2c.chunk.js"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  }
+]);
