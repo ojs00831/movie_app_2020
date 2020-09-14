42105306c3eedb18113c7ac0220f2e2c463eab6a
ojs00831 <ojs00831@naver.com>
2020-09-14 09:27:21 +0900

    detail css 추가 작업2 .

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -8,11 +8,13 @@ function MovieDetail({ id, title, year, rating, runtime, genres, description_ful
             <img src={background_image} alt={title} title={title} />
             <div className="movieDetail_data">
                 <h3 className="movieDetail__title">
+                    title 
+                    <br />
                     {title}
                 </h3>
-                <h5 className="movieDetail__year">{year}</h5>
-                <h5 className="movieDetail__rating">{rating}</h5>
-                <h5 className="movieDetail__runtime">{runtime}</h5>
+                <h5 className="movieDetail__year">year <br /> {year}</h5>
+                <h5 className="movieDetail__rating">rating <br /> {rating}</h5>
+                <h5 className="movieDetail__runtime">runtime <br /> {runtime}</h5>
                 <ul className="movieDetail__genres">
                     {genres.map((genre, index) => {
                         return (
@@ -22,10 +24,10 @@ function MovieDetail({ id, title, year, rating, runtime, genres, description_ful
                         );
                     })}
                 </ul>       
-                <h5 className="movieDetail__description_full">{description_full}</h5>
-                <h5 className="movieDetail__language">{language}</h5>
-                <h5 className="movieDetail__mpa_rating">{mpa_rating}</h5>
-                <ul className="movieDetail__torrents">
+                <h5 className="movieDetail__description_full">description <br /> {description_full}</h5>
+                <h5 className="movieDetail__language">language <br /> {language}</h5>
+                <h5 className="movieDetail__mpa_rating">mpa_rating <br /> {mpa_rating}</h5>
+                <ul className="movieDetail__torrents">torrent <br /> 
                     {torrents.map((torrent, index) => {
                         return (
                             <div key={index}>
